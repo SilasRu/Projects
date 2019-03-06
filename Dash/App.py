86227e930ca3832c9fb11ead6097e8e7722ab013
rudolsil <rudolsil@students.zhaw.ch>
2019-03-06 22:58:14 +0100

    Portfolio value and training set visualization

--- Dash/App.py
+++ Dash/App.py
@@ -1,16 +1,17 @@
 # =============================================================================
 # Dash GUI
 # TODO: 
-#
-#
-#
-#
+# implement Trading markers
+# Model visualization
+# Sharpe
+# Portfolio value
 # =============================================================================
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table_experiments as dt
+import plotly.graph_objs as go
 import pandas as pd
 import os
 from dash.dependencies import Input, Output
@@ -18,18 +19,27 @@ from dash.dependencies import Input, Output
 # =============================================================================
 # Importing Datasets
 # =============================================================================
-filedir = os.path.abspath('D:\\GitHub\\Projects\\Dash\\Data')
-IBM_df_name = 'daily_IBM.csv'
-MSFT_df_name = 'daily_MSFT.csv'
-QCOM_df_name = 'daily_QCOM.csv'
+filedir = os.path.abspath('D:\\GitHub\\Projects\\Dash\\Data\\')
 
-IBM_filepath = os.path.join(filedir, IBM_df_name)
-MSFT_filepath = os.path.join(filedir, MSFT_df_name)
-QCOM_filepath = os.path.join(filedir, QCOM_df_name)
+IBM_df = pd.read_csv(filedir + '\\daily_IBM.csv')
+MSFT_df = pd.read_csv(filedir + '\\daily_MSFT.csv')
+QCOM_df = pd.read_csv(filedir + '\\daily_QCOM.csv')
 
-IBM_df = pd.read_csv(IBM_filepath)
-MSFT_df = pd.read_csv(MSFT_filepath)
-QCOM_df = pd.read_csv(QCOM_filepath)
+portfolio_val_train = pd.read_csv(filedir + '\\portfolio_val_train.csv')
+portfolio_val_train = portfolio_val_train.rename(index=str, columns={'0': 'IBM_stock','1': 'MSFT_stock', '2': 'QCOM_stock', 
+                                              '3': 'Cash', '4': 'Portfolio_val', '5': 'Perc_diff' })
+portfolio_val_test = pd.read_csv(filedir + '\\portfolio_val_test.csv')
+portfolio_val_test = portfolio_val_test.rename(index=str, columns={'0': 'IBM_stock','1': 'MSFT_stock', '2': 'QCOM_stock', 
+                                              '3': 'Cash', '4': 'Portfolio_val', '5': 'Perc_diff' })
+
+train_data = pd.read_csv(filedir + '\\train_data.csv')
+train_data = train_data.transpose()
+train_data = train_data.rename(index= str, columns={0: 'IBM', 1: 'MSFT', 2: 'QCOM'})
+test_data = pd.read_csv(filedir + '\\train_data.csv')
+test_data = test_data.transpose()
+test_data = test_data.rename(index= str, columns={0: 'IBM', 1: 'MSFT', 2: 'QCOM'})
+
+
 
 dataframes= {'IBM': IBM_df,
              'MSFT': MSFT_df,
@@ -91,15 +101,16 @@ app.layout = html.Div(
 # =============================================================================
 # 1. Tab
 # =============================================================================
-                dcc.Tab(label='Tab one',children=[
+                dcc.Tab(label='Data Visualization',children=[
                     #### 3.Row ####                
                     html.Div([
-                            # Dropdown, top left
+                            # Stock Dropdown, top left
                             dcc.Dropdown(
                                     id='stock_dropdown',
                                     options=[{'label': df, 'value': df} for df in dataframes],
                                     value='',
                                     className= 'three columns'),
+                            # Column Dropdown, top left                                    
                             dcc.Dropdown(
                                     id='column_dropdown',
                                     options=[{'label': column, 'value': column} for column in indexes],
@@ -132,16 +143,54 @@ app.layout = html.Div(
 # =============================================================================
 # 2. Tab
 # =============================================================================
-             dcc.Tab(label='Tab two', children=[
-                html.Div([
-                    html.H1(
-                        children='Reinforcement Learning')
-                        ])
+             dcc.Tab(label='Performance Metrics', children=[
+                    #### 3.Row ####                
+                    html.Div([
+                            # Infobox, top right
+                            html.Div(
+                                children= 'Select the stock and index for visualization',
+                                className='six columns',
+                                style={
+                                    'float': 'right'})
+                            ], className="row",style={'marginTop': 20}),
+                    #### 4. Row ####
+                    html.Div([
+                        html.Div([
+                            # Graph
+                            dcc.Graph(
+                                id='stock_ammount',
+                                figure={    
+                                    'data':[                                        
+                                        {'x': train_data.index, 'y': train_data['IBM'],
+                                         'type': 'line', 'name': 'IBM'},
+                                        {'x': train_data.index, 'y': train_data['QCOM'],
+                                         'type': 'line', 'name': 'QCOM','visible': 'legendonly'},
+                                        {'x': train_data.index, 'y': train_data['MSFT'],
+                                         'type': 'line', 'name': 'MSFT', 'visible': 'legendonly'},
+                                        {'x': portfolio_val_train.index, 'y': portfolio_val_train['IBM_stock'],
+                                         'type': 'scatter','opacity': 0.4, 'name': 'IBM_stock'},
+                                        {'x': portfolio_val_train.index, 'y': portfolio_val_train['QCOM_stock'],
+                                         'type': 'scatter','opacity': 0.4, 'name': 'QCOM_stock','visible': 'legendonly'},
+                                        {'x': portfolio_val_train.index, 'y': portfolio_val_train['MSFT_stock'],
+                                         'type': 'scatter','opacity': 0.4, 'name': 'MSFT_stock','visible': 'legendonly'}
+                                         ]
+                                        })
+                                        
+                                ],className= 'twelve columns'),
+#                        html.Div([
+#                            # Table
+#                            dt.DataTable(
+#                                    id='table',
+#                                    rows = [{}],
+#                                    filterable = True,
+#                                    sortable= True),
+#                                ], className= 'twelve columns')
+                            ], className="row")
                     ]),
 # =============================================================================
 # 3. Tab
 # =============================================================================
-             dcc.Tab(label='Tab three', children=[
+             dcc.Tab(label='Information', children=[
                 html.Div([
                     html.H1(
                         children='Reinforcement Learning')
